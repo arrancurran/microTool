71ec03dfca076c92ce18f49d19de159a97280c92
arrancurran <arrancurran@gmail.com>
2025-03-11 11:47:04 +0100

    color

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ def calc_img_hist(self, img_data):
         self.hist_display.figure.clear()
         ax = self.hist_display.figure.add_subplot(111)
         ax.plot(hist, color='#97c1ff', linestyle='-', linewidth=0.5, marker='')
-        ax.set_facecolor('#2c3036')
+        # ax.set_facecolor('#2c3036')
         ax.set_yscale('log')
         ax.set_xlim([0, 256])
         ax.set_xticks([])
@@ -16,7 +16,7 @@ def calc_img_hist(self, img_data):
         for spine in ax.spines.values():
             spine.set_visible(False)
         ax.set_frame_on(False)
-        self.hist_display.figure.patch.set_facecolor('#25292E')
+        self.hist_display.figure.patch.set_facecolor('#2f353c')
         
         self.hist_display.figure.subplots_adjust(left=0, right=1, top=1, bottom=0)
 
