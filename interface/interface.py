0858acfb0d0c75f7457e436e27ae0eda85a06434
arrancurran <arrancurran@gmail.com>
2025-03-13 18:29:39 +0100

    global ui

--- interface/interface.py
+++ interface/interface.py
@@ -1,24 +1,23 @@
 from PyQt6.QtWidgets import (
-    QApplication, QMainWindow, QLabel, QWidget, QSlider, QHBoxLayout, QGridLayout, QSizePolicy,
-    QSpinBox, QGroupBox, QVBoxLayout, QFormLayout, QToolBar,QWidgetAction
-)
-from PyQt6.QtGui import QPixmap, QImage, QAction
-from PyQt6.QtCore import Qt, QTimer, QSize
+    QMainWindow, QLabel, QWidget, QSlider, QHBoxLayout, QSizePolicy,
+    QSpinBox, QGroupBox, QVBoxLayout, QFormLayout, QToolBar)
+from PyQt6.QtGui import QAction
+from PyQt6.QtCore import Qt, QSize
 # Run qta-browser from terminal to see all available icons
 import qtawesome as qta, matplotlib.pyplot as plt
 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
-class MainWindow(QMainWindow):
+class ui_layout(QMainWindow):
     def __init__(self):
         super().__init__()
         
-        self.ui_layout()
+        self.setup_ui()
 
-    def ui_layout(self):
+    def setup_ui(self):
         # Set initial window size
         self.resize(1200, 800)
-        self.setWindowTitle('MainWindow')
+        self.setWindowTitle('ColloidCam')
         self.setStyleSheet("background-color: #25292E;")
         
         # Creates the central widget
@@ -28,26 +27,30 @@ class MainWindow(QMainWindow):
         MainWindow_layout.setContentsMargins(10, 10, 10, 10)
         MainWindow_layout.setSpacing(10)
         
-        # Create and configure the camera view
-        self.view = QLabel(self)
-        self.view.setMinimumSize(400, 300)
-        self.view.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        
         # Create controls container widget with horizontal layout
         controls_container = QWidget()
         controls_layout = QHBoxLayout(controls_container)
         controls_layout.setContentsMargins(0, 0, 0, 0)
         controls_layout.setSpacing(10)
         
-        # Left column for ROI controls
+        # Create and configure the camera view
+        self.view = QLabel(self)
+        self.view.setMinimumSize(400, 300)
+        self.view.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        
+        # ROI controls
         roi_group = QGroupBox("Region of Interest")
         roi_layout = QFormLayout()
         
-        # Create ROI control spinboxes
         self.roi_width = QSpinBox()
         self.roi_height = QSpinBox()
         self.roi_offset_x = QSpinBox()
         self.roi_offset_y = QSpinBox()
+        
+                
+        # Right column for histogram and exposure
+        right_column = QWidget()
+        right_layout = QVBoxLayout(right_column)
         
          # Add ROI controls to layout
         roi_layout.addRow("Width:", self.roi_width)
@@ -55,11 +58,7 @@ class MainWindow(QMainWindow):
         roi_layout.addRow("Offset X:", self.roi_offset_x)
         roi_layout.addRow("Offset Y:", self.roi_offset_y)
         roi_group.setLayout(roi_layout)
-        
-        # Right column for histogram and exposure
-        right_column = QWidget()
-        right_layout = QVBoxLayout(right_column)
-        
+
         # Configure histogram
         self.hist_display = FigureCanvas(plt.figure())
         self.hist_display.setMinimumWidth(512)
@@ -67,13 +66,11 @@ class MainWindow(QMainWindow):
         self.hist_display.setMinimumHeight(120)
         self.hist_display.setMaximumHeight(120)
         
-        # Configure exposure slider
+         # Configure exposure slider
         self.exposure_slider = QSlider(Qt.Orientation.Horizontal)
-        
-         # Configure exposure label
         self.exposure_label = QLabel()
         self.exposure_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        
+       
          # Add widgets to right column
         right_layout.addWidget(self.hist_display)
         right_layout.addWidget(self.exposure_slider)
@@ -90,11 +87,31 @@ class MainWindow(QMainWindow):
         controls_layout.addWidget(right_column)
         
         # Add  sections to main layout with proper ratios
-        ColloidCamUI_layout.addWidget(self.view, 2)
-        ColloidCamUI_layout.addWidget(controls_container, 1)
+        MainWindow_layout.addWidget(self.view, 2)
+        MainWindow_layout.addWidget(controls_container, 1)
         
         # Create toolbar
-        tollbar_height = 28
+        toolbar_height = 32
+        icon_size = QSize(toolbar_height, toolbar_height)*0.6
         toolbar = QToolBar("Main Toolbar")
-        toolbar.setIconSize(QSize(tollbar_height, tollbar_height))
+        toolbar.setStyleSheet("background-color: #333;")  # Set toolbar style
+        toolbar.setIconSize(icon_size)
         self.addToolBar(Qt.ToolBarArea.TopToolBarArea, toolbar)
+        
+        # Add play action
+        play_action = QAction(qta.icon('fa5s.play'), "Play", self)
+        play_action.triggered.connect(self.start_live_display)
+        toolbar.addAction(play_action)
+
+        # Add stop action
+        stop_action = QAction(qta.icon('fa5s.stop'), "Stop", self)
+        stop_action.triggered.connect(self.stop_live_display)
+        toolbar.addAction(stop_action)
+
+        # Add record action
+        record_action = QAction(qta.icon('fa5.dot-circle'), "Record", self)
+        record_action.triggered.connect(self.start_recording)
+        toolbar.addAction(record_action)
+
+        # Set toolbar size
+        toolbar.setFixedHeight(toolbar_height)
--- main.py
+++ main.py
@@ -1,25 +1,18 @@
-import sys, cv2, matplotlib.pyplot as plt
+import sys, cv2
 
-from PyQt6.QtWidgets import (
-    QApplication, QMainWindow, QLabel, QWidget, QSlider, QHBoxLayout, QGridLayout, QSizePolicy,
-    QSpinBox, QGroupBox, QVBoxLayout, QFormLayout, QToolBar,QWidgetAction
-)
-from PyQt6.QtGui import QPixmap, QImage, QAction
-from PyQt6.QtCore import Qt, QTimer, QSize
+from PyQt6.QtWidgets import QApplication
+from PyQt6.QtGui import QPixmap, QImage
+from PyQt6.QtCore import Qt, QTimer
 # Run qta-browser from terminal to see all available icons
-import qtawesome as qta
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-
 import utils
-
-from interface.interface import MainWindow
+from interface.interface import ui_layout
 from instruments.camera import Camera
 from interface.camera import CameraUI
 
 # UI class that inherits from QMainWindow
-class ColloidCamUI(QMainWindow):
+class ColloidCamUI(ui_layout):
     def __init__(self):
-        super().__init__()
+        ui_layout.__init__(self)
         
         self.instruments()
         self.setupUI()
@@ -27,66 +20,38 @@ class ColloidCamUI(QMainWindow):
     def instruments(self):
         # init cam and get cam_meta
         self.active_camera = Camera()
-        self.active_camera.start_cam()
+        # self.active_camera.start_cam()
         self.cam_meta = self.active_camera.get_cam_cam_meta()
     
     def setupUI(self):
-
-        
-        self.main_window = MainWindow()  # Instantiate MainWindow
-        
         # Create ROI control spinboxes
-
-        MainWindow.roi_width.setRange(self.cam_meta['width_min'], self.cam_meta['width_max'])
-        MainWindow.roi_width.setSingleStep(self.cam_meta['width_inc'])
-        MainWindow.roi_width.setValue(self.cam_meta['width'])
-        MainWindow.roi_width.valueChanged.connect(self.update_roi)
+        self.roi_width.setRange(self.cam_meta['width_min'], self.cam_meta['width_max'])
+        self.roi_width.setSingleStep(self.cam_meta['width_inc'])
+        self.roi_width.setValue(self.cam_meta['width'])
+        self.roi_width.valueChanged.connect(self.update_roi)
         
+        self.roi_height.setRange(self.cam_meta['height_min'], self.cam_meta['height_max'])
+        self.roi_height.setSingleStep(self.cam_meta['height_inc'])
+        self.roi_height.setValue(self.cam_meta['height'])
+        self.roi_height.valueChanged.connect(self.update_roi)
+        self.roi_height.setValue(self.cam_meta['height'])
+        self.roi_height.valueChanged.connect(self.update_roi)
         
-        MainWindow.roi_height.setRange(self.cam_meta['height_min'], self.cam_meta['height_max'])
-        MainWindow.roi_height.setSingleStep(self.cam_meta['height_inc'])
-        MainWindow.roi_height.setValue(self.cam_meta['height'])
-        MainWindow.roi_height.valueChanged.connect(self.update_roi)
+        self.roi_offset_x.setRange(0, 4000)
+        self.roi_offset_x.setValue(0)
+        self.roi_offset_x.valueChanged.connect(self.update_roi)
         
+        self.roi_offset_y.setRange(0, 4000)
+        self.roi_offset_y.setValue(0)
+        self.roi_offset_y.valueChanged.connect(self.update_roi)
         
-        MainWindow.roi_offset_x.setRange(0, 4000)
-        MainWindow.roi_offset_x.setValue(0)
-        MainWindow.roi_offset_x.valueChanged.connect(self.update_roi)
-        
-        
-        MainWindow.roi_offset_y.setRange(0, 4000)
-        MainWindow.roi_offset_y.setValue(0)
-        MainWindow.roi_offset_y.valueChanged.connect(self.update_roi)
-        
-        
-        
-        
-        MainWindow.exposure_slider.setRange(self.cam_meta['min_exposure'], self.cam_meta['max_exposure'])
-        MainWindow.exposure_slider.setValue(self.cam_meta['min_exposure'])
-        MainWindow.exposure_slider.setTickInterval((self.cam_meta['max_exposure'] - self.cam_meta['min_exposure']) // 10)
-        MainWindow.exposure_slider.valueChanged.connect(self.set_exposure)
-       
+        self.exposure_slider.setRange(self.cam_meta['min_exposure'], self.cam_meta['max_exposure'])
+        self.exposure_slider.setValue(self.cam_meta['min_exposure'])
+        self.exposure_slider.setTickInterval((self.cam_meta['max_exposure'] - self.cam_meta['min_exposure']) // 10)
+        self.exposure_slider.valueChanged.connect(self.set_exposure)
        
         self.set_exposure(self.cam_meta['min_exposure'])
         
-
-        # Add play action
-        play_action = QAction(qta.icon('fa5s.play'), "Play", self)
-        play_action.triggered.connect(self.start_live_display)
-        toolbar.addAction(play_action)
-
-        # Add stop action
-        stop_action = QAction(qta.icon('fa5s.stop'), "Stop", self)
-        stop_action.triggered.connect(self.stop_live_display)
-        toolbar.addAction(stop_action)
-
-        # Add record action
-        record_action = QAction(qta.icon('ei.record'), "Record", self)
-        toolbar.addAction(record_action)
-
-        # Set toolbar size
-        toolbar.setFixedHeight(tollbar_height)
-
         self.running = False
         self.show()
 
@@ -140,6 +105,7 @@ class ColloidCamUI(QMainWindow):
     def start_live_display(self):
   
         try:
+            self.active_camera.start_cam()
             self.update_image() # Attempt to start the camera
         except Exception as e:
             print(f"Camera start error: {e}")
@@ -151,6 +117,9 @@ class ColloidCamUI(QMainWindow):
         if self.running:
             self.running = False
             self.active_camera.stop_cam()
+
+    def start_recording(self):
+        print(f"Recording started")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
