e4ab54479d5747862254b060324804ca7fbb7add
arrancurran <arrancurran@gmail.com>
2025-03-16 23:18:43 +0100

    update hist

--- utils.py
+++ utils.py
@@ -21,6 +21,7 @@ def calc_img_hist(self, image_data):
     for spine in ax.spines.values():
         spine.set_visible(False)
     ax.set_frame_on(False)
+    
     self.hist_display.figure.patch.set_facecolor('#2f353c')
     
     self.hist_display.figure.subplots_adjust(left=0, right=1, top=1, bottom=0)
