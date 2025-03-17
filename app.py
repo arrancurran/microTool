cc0fae2c56cdf9cc79341352a860894a7c8afb1e
arrancurran <arrancurran@gmail.com>
2025-03-17 10:05:04 +0100

    fusion theme

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from PyQt6.QtWidgets import QApplication
 from interface.ui_scaffolding import uiScaffolding
 
 app = QApplication(sys.argv)
+app.setStyle("Fusion")
 interface = uiScaffolding()
 interface.show()
 sys.exit(app.exec())
