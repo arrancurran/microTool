60744114d30df7f38519734fb35ef9b142fd6ee4
Arran Curran <arrancurran@gmail.com>
2025-03-30 15:36:03 +0200

    [minor] widgets then build

--- interface/ui.py
+++ interface/ui.py
@@ -17,41 +17,43 @@ class ui(QMainWindow):
 
         super().__init__()
         self.ui_scaffolding = self.load_ui_scaffolding('ui_scaffolding.json')
+        ### TOOLBAR ###
+        self.setup_toolbar()
+
+        ### IMAGE CONTAINER ###
+        # image_container = self.create_image_container()
+        
+        ### HISTOGRAM ### 
+        self.setup_histogram()
+        
+        ### EXPOSURE SLIDER ###
+        self.setup_exposure_slider()
         
         ### STATUS BAR ###
         self.setup_status_bar()
         
-        ### HISTOGRAM ### 
-        self.setup_histogram()
-       
-        ###Build the rest of the UI###
+        ### Build the rest of the UI ###
         self.build_ui()
         
         # self.apply_styles()  # Apply styling after UI is built
-
-    def load_ui_scaffolding(self, file_path):
-        with open(os.path.join('interface', file_path), 'r') as f:
-            return json.load(f)
 
     # def apply_styles(self):
     #     with open(os.path.join('interface', "style.css"), "r") as f:
     #         self.setStyleSheet(f.read())
     
-    def setup_status_bar(self):
-        self.status_bar = QStatusBar()
-        self.setStatusBar(self.status_bar)
-        
-        for label_name, label_text in self.ui_scaffolding['status_bar']['labels'].items():
-            label = QLabel()
-            setattr(self, f"{label_name}_label", label)
-            self.status_bar.addWidget(label)
-            label.setText(label_text)
-       
-    def setup_histogram(self):
-        self.hist_display = pg.PlotWidget()
-        self.hist_display.setFixedSize(512, 120)
-        self.histogram_plot = ImgHistDisplay(self.hist_display)
+    def load_ui_scaffolding(self, file_path):
+        with open(os.path.join('interface', file_path), 'r') as f:
+            return json.load(f)
     
+    def setup_toolbar(self):
+        toolbar = QToolBar("Main Toolbar")
+        self.addToolBar(Qt.ToolBarArea.TopToolBarArea, toolbar)
+        # Add actions
+        for action_name, action_data in self.ui_scaffolding['toolbar']['icons'].items():
+            action_obj = QAction(qta.icon(action_data['icon']), action_name, self)
+            action_obj.setToolTip(action_data['tooltip'])
+            toolbar.addAction(action_obj)
+            setattr(self, action_data['cmd'], action_obj)
     
     def create_image_container(self):
         image_container = DispMouseHandler(self)
@@ -60,7 +62,6 @@ class ui(QMainWindow):
         image_container.setAlignment(Qt.AlignmentFlag.AlignCenter)
         image_container.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
         return image_container
-    
     
     def create_roi_controls(self):
         roi_group = QGroupBox("Region of Interest")
@@ -82,7 +83,7 @@ class ui(QMainWindow):
         roi_form_layout.addRow("Offset Y:", self.roi_offset_y)
         roi_layout.addLayout(roi_form_layout)
         
-        ### Add buttons below ROI controls ###
+        # Add buttons below ROI controls
         button_container = QWidget()
         button_layout = QVBoxLayout(button_container)
         button_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
@@ -100,14 +101,19 @@ class ui(QMainWindow):
         roi_group.setSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
         return roi_group
     
-    def create_right_column(self):
-        """Create and configure the right column with histogram and exposure slider."""
-        right_column = QWidget()
-        right_layout = QVBoxLayout(right_column)
-
+    def setup_histogram(self):
+        self.hist_display = pg.PlotWidget()
+        self.hist_display.setFixedSize(512, 120)
+        self.histogram_plot = ImgHistDisplay(self.hist_display)
+    
+    def setup_exposure_slider(self):
         self.exposure_slider = QSlider(Qt.Orientation.Horizontal)
         self.exposure_label = QLabel()
         self.exposure_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+    
+    def create_right_column(self):
+        right_column = QWidget()
+        right_layout = QVBoxLayout(right_column)
 
         right_layout.addWidget(self.hist_display)
         right_layout.addWidget(self.exposure_slider)
@@ -116,23 +122,31 @@ class ui(QMainWindow):
 
         right_column.setSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
         return right_column
-    
+            
+    def setup_status_bar(self):
+        self.status_bar = QStatusBar()
+        self.setStatusBar(self.status_bar)
+        
+        for label_name, label_text in self.ui_scaffolding['status_bar']['labels'].items():
+            label = QLabel()
+            setattr(self, f"{label_name}_label", label)
+            self.status_bar.addWidget(label)
+            label.setText(label_text)
     
     def build_ui(self):
-        
-        ### Main Window ###
         # Using a QWidget as the central widget inside QMainWindow helps separate the layout 
         # and content of the main window from the QMainWindow.
         central_widget = QWidget(self)
         self.setCentralWidget(central_widget)
         central_widget_layout = QHBoxLayout(central_widget)
 
-         # Image container
+         ### IMAGE CONTAINER ###
         self.image_container = self.create_image_container()
         
-        ### Controls ###
+        ### CONTROLS CONTAINER ###
         controls_container = QWidget()
         controls_layout = QHBoxLayout(controls_container)
+        
         
         # Add ROI controls and right column to controls layout
         roi_group = self.create_roi_controls()
@@ -140,25 +154,9 @@ class ui(QMainWindow):
         controls_layout.addWidget(roi_group)
         controls_layout.addWidget(right_column)
        
-        ### Add Columns to Controls Layout ###
-        controls_layout.addWidget(roi_group)
-        controls_layout.addWidget(right_column)
         
-        ### Add Sections to Main Layout with Proper Ratios ###
+        # Add Sections to Main Layout with Proper Ratios
         central_widget_layout.addWidget(self.image_container, 2)
         central_widget_layout.addWidget(controls_container, 1)
         
-        self.setup_toolbar()
-        self.setWindowTitle(self.ui_scaffolding['app']['name'])
-        
-    def setup_toolbar(self):
-        ### Toolbar ###
-        toolbar = QToolBar("Main Toolbar")
-        self.addToolBar(Qt.ToolBarArea.TopToolBarArea, toolbar)
-
-        ### Add toolbar actions ###
-        for action_name, action_data in self.ui_scaffolding['toolbar']['icons'].items():
-            action_obj = QAction(qta.icon(action_data['icon']), action_name, self)
-            action_obj.setToolTip(action_data['tooltip'])
-            toolbar.addAction(action_obj)
-            setattr(self, action_data['cmd'], action_obj)
+        self.setWindowTitle(f"{self.ui_scaffolding['app']['name']} - {self.ui_scaffolding['app']['version']}")
